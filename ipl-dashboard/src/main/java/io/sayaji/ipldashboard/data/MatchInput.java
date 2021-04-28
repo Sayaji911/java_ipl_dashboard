a21161059f6ca8f8d97a89c6f005f90e1997cfba
Sayaji911 <sayajichavan9999@gmail.com>
2021-04-29 02:48:57 +0530

    first push

---
+++ ipl-dashboard/src/main/java/io/sayaji/ipldashboard/data/MatchInput.java
@@ -0,0 +1,25 @@
+package io.sayaji.ipldashboard.data;
+
+public class MatchInput {
+    city
+    date
+    player_of_match
+    venue
+    neutral_venue
+    team1
+    team2
+    toss_winner
+    toss_decision
+    winner
+    result
+    result_margin
+    eliminator
+    method
+    umpire1
+    umpire2
+    id
+
+
+
+
+}
